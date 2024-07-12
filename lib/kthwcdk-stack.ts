86b62b69e4a85af3d8a25787d158f3504007e695
Kaustubh Khavnekar <kaustubhkhavnekar@gmail.com>
2024-07-11 23:34:20 -0400

    Userdata update to use kind on single server

--- lib/kthwcdk-stack.ts
+++ lib/kthwcdk-stack.ts
@@ -24,26 +24,39 @@ export class KthwcdkStack extends cdk.Stack {
     const keyPair = ec2.KeyPair.fromKeyPairName(this, 'KubeKeyPair', 'KubeKeyPair');
     const userData = ec2.UserData.forLinux()
     userData.addCommands(
-      'mkdir /tmp/ssm',
-      'cd /tmp/ssm',
+      'mkdir /tmp/userdata',
+      'cd /tmp/userdata',
       `wget https://s3.${cdk.Stack.of(this).region}.amazonaws.com/amazon-ssm-${cdk.Stack.of(this).region}/latest/debian_arm64/amazon-ssm-agent.deb`,
       'sudo dpkg -i amazon-ssm-agent.deb',
       'sudo systemctl enable amazon-ssm-agent',
       'sudo systemctl start amazon-ssm-agent',
       'curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_arm64/session-manager-plugin.deb" -o "session-manager-plugin.deb"',
       'sudo dpkg -i session-manager-plugin.deb',
+      'wget https://go.dev/dl/go1.22.5.linux-arm64.tar.gz',
+      'sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.5.linux-arm64.tar.gz',
+      'sudo apt-get update',
+      'sudo apt-get install -y ca-certificates curl',
+      'sudo install -m 0755 -d /etc/apt/keyrings',
+      'sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc',
+      'sudo chmod a+r /etc/apt/keyrings/docker.asc',
+      'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null',
+      'sudo apt-get update',
+      'sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin',
+      'curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.23.0/kind-linux-arm64',
+      'chmod +x ./kind',
+      'sudo mv ./kind /usr/local/bin/kind'
     );
-    const jumpboxRootVolume: ec2.BlockDevice = {
+    const mainServerRootVolume: ec2.BlockDevice = {
       deviceName: '/dev/xvda',
       volume: ec2.BlockDeviceVolume.ebs(10),
     };
-    const jumpbox = new ec2.Instance(this, 'jumpbox', {
+    new ec2.Instance(this, 'mainServer', {
       vpc: vpc,
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.A1, ec2.InstanceSize.MEDIUM),
       machineImage: ami,
       associatePublicIpAddress: true,
       allowAllOutbound: true,
-      instanceName: 'jumpbox',
+      instanceName: 'mainServer',
       requireImdsv2: true,
       ssmSessionPermissions: true,
       vpcSubnets: {
@@ -51,97 +64,8 @@ export class KthwcdkStack extends cdk.Stack {
       },
       userData: userData,
       keyPair: keyPair,
-      blockDevices: [jumpboxRootVolume],
+      blockDevices: [mainServerRootVolume],
     });
-    jumpbox.addToRolePolicy(new iam.PolicyStatement({
-      actions: ['ssm:StartSession'],
-      effect: iam.Effect.ALLOW,
-      resources: ['*'],
-    }));
-    const rootVolume: ec2.BlockDevice = {
-      deviceName: '/dev/xvda',
-      volume: ec2.BlockDeviceVolume.ebs(20),
-    };
-    const server = new ec2.Instance(this, 'server', {
-      vpc: vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.A1, ec2.InstanceSize.MEDIUM),
-      machineImage: ami,
-      associatePublicIpAddress: true,
-      allowAllOutbound: true,
-      instanceName: 'server',
-      requireImdsv2: true,
-      ssmSessionPermissions: true,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC,
-      },
-      userData: userData,
-      keyPair: keyPair,
-      blockDevices: [rootVolume],
-    });
-    const node0 = new ec2.Instance(this, 'node0', {
-      vpc: vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.A1, ec2.InstanceSize.MEDIUM),
-      machineImage: ami,
-      associatePublicIpAddress: true,
-      allowAllOutbound: true,
-      instanceName: 'node-0',
-      requireImdsv2: true,
-      ssmSessionPermissions: true,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC,
-      },
-      userData: userData,
-      keyPair: keyPair,
-      blockDevices: [rootVolume],
-    });
-    const node1 = new ec2.Instance(this, 'node1', {
-      vpc: vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.A1, ec2.InstanceSize.MEDIUM),
-      machineImage: ami,
-      associatePublicIpAddress: true,
-      allowAllOutbound: true,
-      instanceName: 'node-1',
-      requireImdsv2: true,
-      ssmSessionPermissions: true,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC,
-      },
-      userData: userData,
-      keyPair: keyPair,
-      blockDevices: [rootVolume],
-    });
-
-    jumpbox.connections.allowFrom(server.connections, ec2.Port.allTraffic());
-    jumpbox.connections.allowFrom(node0.connections, ec2.Port.allTraffic());
-    jumpbox.connections.allowFrom(node1.connections, ec2.Port.allTraffic());
-    jumpbox.connections.allowFrom(ec2.Peer.ipv4(publicIpCidr), ec2.Port.SSH);
-
-
-    server.connections.allowFrom(jumpbox.connections, ec2.Port.allTraffic());
-    server.connections.allowFrom(node0.connections, ec2.Port.allTraffic());
-    server.connections.allowFrom(node1.connections, ec2.Port.allTraffic());
-    server.connections.allowFrom(ec2.Peer.ipv4(publicIpCidr), ec2.Port.SSH);
-
-
-    node0.connections.allowFrom(jumpbox.connections, ec2.Port.allTraffic());
-    node0.connections.allowFrom(server.connections, ec2.Port.allTraffic());
-    node0.connections.allowFrom(node1.connections, ec2.Port.allTraffic());
-    node0.connections.allowFrom(ec2.Peer.ipv4(publicIpCidr), ec2.Port.SSH);
-
-
-    node1.connections.allowFrom(jumpbox.connections, ec2.Port.allTraffic());
-    node1.connections.allowFrom(server.connections, ec2.Port.allTraffic());
-    node1.connections.allowFrom(node0.connections, ec2.Port.allTraffic());
-    node1.connections.allowFrom(ec2.Peer.ipv4(publicIpCidr), ec2.Port.SSH);
-
-    const script = `tmux split-window -h  "ssh -J admin@${jumpbox.instancePublicDnsName} admin@${server.instancePublicDnsName}"
-    tmux select-layout tiled > /dev/null
-    tmux split-window -h  "ssh -J admin@${jumpbox.instancePublicDnsName} admin@${node0.instancePublicDnsName}"
-    tmux select-layout tiled > /dev/null
-    tmux split-window -h  "ssh -J admin@${jumpbox.instancePublicDnsName} admin@${node1.instancePublicDnsName}"
-    tmux select-layout tiled > /dev/null
-    tmux select-pane -t 0
-    ssh -A admin@${jumpbox.instancePublicDnsName}`;
-    new cdk.CfnOutput(this, 'connectScript', { value: script });
+    // mainServer.connections.allowFrom(ec2.Peer.ipv4(publicIpCidr), ec2.Port.SSH);
   }
 }
